63f40a3be69a3a79d667c1961e336441993f42d2
Cole Sammons <colemsammons@gmail.com>
2021-09-24 16:00:37 -0700

    react build

---
+++ client/build/precache-manifest.41ea6e9b58a611d6033d55253d20e43b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dc7bb6391684476d83bc65941213147",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a414a7cb70693bc5411d",
+    "url": "/static/css/main.be2b856d.chunk.css"
+  },
+  {
+    "revision": "2762f476089e6fb871f0",
+    "url": "/static/js/2.84bb8d9c.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.84bb8d9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a414a7cb70693bc5411d",
+    "url": "/static/js/main.a08d4d13.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
